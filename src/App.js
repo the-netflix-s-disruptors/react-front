881b4e561d566da6cbc812568f7133ee8199bb84
rcrestey <rcrestey@student.42.fr>
2020-02-19 21:08:21 +0100

    User part done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
-function App() {
-  return <div className="App"></div>;
+import "./App.css";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import { AuthContext } from "./context/auth";
+import PrivateRoute from "./PrivateRoute";
+import ResetPassword from "./pages/PassReset";
+import ResetingPassword from "./pages/PassResetConf";
+import UserEdition from "./pages/UserEdition";
+
+function App(props) {
+  const [authTokens, setAuthTokens] = useState();
+
+  const setTokens = data => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  };
+
+  return (
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home Page</Link>
+            </li>
+            <li>
+              <Link to="/sign-in">Sign In</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/preferences" component={UserEdition} />
+          {/* <PrivateRoute exact path="/" component={Home} /> */}
+          <Route exact path="/sign-in" component={SignIn} />
+          <Route path="/sign-in/:uuid/:token" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+          <Route path="/password-reset" component={ResetPassword} />
+          <Route
+            path="/password-reseting/:uuid/:token"
+            component={ResetingPassword}
+          />
+        </div>
+      </Router>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,2 +1,26 @@
 import React from "react";
-import AppBar from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+import { useAuth } from "../context/auth";
+
+const NavBar = () => {
+  const { setAuthTokens } = useAuth();
+
+  function logOut() {
+    setAuthTokens();
+  }
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography color="inherit">HYPERTUBE</Typography>
+          <Button onClick={logOut}> LOG OUT</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+export default NavBar;
